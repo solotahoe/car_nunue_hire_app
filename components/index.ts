f157b55f157fd1c16cdfb0d53e24f54a91dfa1db
solotahoe <githirasolomon@gmail.com>
2024-05-08 20:35:27 +0300

    first commit

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import CustomFilter from "./CustomFIlter";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+
+import SearchBar from "./SearchBar";
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    CarCard,
+    ShowMore
+}
